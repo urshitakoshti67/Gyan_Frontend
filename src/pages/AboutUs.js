a9e96ffeb46a8b23530083ff4782d9369d1bce91
Aarush-Kinhikar-0209 <aarush.kinhikar@gmail.com>
2023-12-27 10:28:42 +0530

    enabled about us route and commented previous interns part

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,40 +7,40 @@ import Footer from '../components/Footer';
 import about from '../images/about.png';
 import { useSelector } from 'react-redux';
 const AboutUs = () => {
-  const {isAdmin} = useSelector((state)=>{return state.user});
+  const { isAdmin } = useSelector((state) => { return state.user });
   return (
     <div>
-      <NavBar isAdmin={isAdmin}/>
+      <NavBar isAdmin={isAdmin} />
       <Heading mx="10" fontSize="6xl">
         About Us
       </Heading>
       <Flex
-  mt={['10', '10', '24']}
-  overflowX={'auto'}
-  direction={['column', 'column', 'row']}
->
-  <Text
-    mx="auto"
-    maxW={['100%', '100%', '33%']}
-    width={['100%', '100%', '33%']}
-    as="u"
-    fontSize={['3xl', '3xl', '4xl']}
-    color="#02A4FF"
-    textAlign={['center', 'center', 'left']}
-    mb={['4', '4', '0']} // Add margin-bottom for spacing between Text and Image
-  >
-    We believe in fostering the next generation of talent. Our internship program is more than just an opportunity—it's a gateway to hands-on experience, mentorship, and a vibrant community that values innovation, growth, and diversity.
-  </Text>
-  <Image
-    src={about}
-    alt="picture"
-    mx="auto"
-    minW={['100%', '100%', '400px']} // Set minimum width for small screens
-    maxW={['100%', '100%', '55%']} // Set maximum width for larger screens
-    mb={['4', '4', '0']} // Add margin-bottom for spacing between Text and Image
-    order={['2', '2', '1']} // Change order for small and medium screens
-  />
-</Flex>
+        mt={['10', '10', '24']}
+        overflowX={'auto'}
+        direction={['column', 'column', 'row']}
+      >
+        <Text
+          mx="auto"
+          maxW={['100%', '100%', '33%']}
+          width={['100%', '100%', '33%']}
+          as="u"
+          fontSize={['3xl', '3xl', '4xl']}
+          color="#02A4FF"
+          textAlign={['center', 'center', 'left']}
+          mb={['4', '4', '0']} // Add margin-bottom for spacing between Text and Image
+        >
+          We believe in fostering the next generation of talent. Our internship program is more than just an opportunity—it's a gateway to hands-on experience, mentorship, and a vibrant community that values innovation, growth, and diversity.
+        </Text>
+        <Image
+          src={about}
+          alt="picture"
+          mx="auto"
+          minW={['100%', '100%', '400px']} // Set minimum width for small screens
+          maxW={['100%', '100%', '55%']} // Set maximum width for larger screens
+          mb={['4', '4', '0']} // Add margin-bottom for spacing between Text and Image
+          order={['2', '2', '1']} // Change order for small and medium screens
+        />
+      </Flex>
 
       <div>
         <Box bgColor="#BFDEFF" mt="10">
@@ -61,7 +61,7 @@ const AboutUs = () => {
           </Center>
         </Box>
       </div>
-      <PrevInterns />
+      {/* <PrevInterns /> */}
       <Footer />
     </div>
   );
